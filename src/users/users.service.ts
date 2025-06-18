139bc2c803aa6fd66b79771b2cdb2a30146a680f
한상훈 <hzyan2@gmail.com>
2025-06-18 21:02:01 +0900

    feat: 유저 생성 추가

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import * as bcrypt from 'bcrypt';
+import { Repository } from 'typeorm';
+import { CreateUserDto } from './dto/create-user.dto';
+import { User } from './user.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private usersRepository: Repository<User>,
+  ) {}
+
+  async create(createUserDto: CreateUserDto) {
+    const { email, password, role } = createUserDto;
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = this.usersRepository.create({
+      email,
+      password: hashedPassword,
+      role,
+    });
+
+    return this.usersRepository.save(user);
+  }
+}
